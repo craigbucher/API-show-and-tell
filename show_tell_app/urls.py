4913eb8f061c7a10cdef6dc78c2ca5ff9f7209df
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-06-24 08:16:17 -0500

    working

--- show_tell_app/urls.py
+++ show_tell_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="home"),
+    path('', views.index),
+    path('raw', views.raw),
+    path('small', views.small),
+    path('thumb', views.thumb),
 ]
--- show_tell_app/views.py
+++ show_tell_app/views.py
@@ -11,7 +11,7 @@ import random
 def index(request):
 
     pp = pprint.PrettyPrinter(indent=2, depth=2)
-
+    photo_id = request.GET.get('id') or random.randint(1, 9)
     load_dotenv()
     auth = OAuth1(os.environ['apikey'], os.environ['secretkey'])
     pp.pprint(auth)
@@ -19,9 +19,48 @@ def index(request):
     endpoint = f"https://api.unsplash.com/photos/?client_id={apikey}"
     API_response = HTTP_Client.get(endpoint, auth=auth)
     responseJSON = API_response.json()
-    photo = responseJSON[(random.randint(1, 10))]['urls']['small']
+    photo = responseJSON[int(photo_id)]['urls']['small']
     description = responseJSON[0]['description']
-    pp.pprint(description)
+    # pp.pprint(description)
     
     response = render(request, 'pages/index.html', {'preview_url': photo, 'description': description})
+    return response
+
+def raw(request):
+    photo_id = request.GET.get('id') or random.randint(1, 9)
+    load_dotenv()
+    auth = OAuth1(os.environ['apikey'], os.environ['secretkey'])
+    apikey = (os.environ['apikey'])
+    endpoint = f"https://api.unsplash.com/photos/?client_id={apikey}"
+    API_response = HTTP_Client.get(endpoint, auth=auth)
+    responseJSON = API_response.json()
+    photo = responseJSON[int(photo_id)]['urls']['raw']
+    
+    response = render(request, 'pages/index.html', {'preview_url': photo})
+    return response
+
+def small(request):
+    photo_id = request.GET.get('id') or random.randint(1, 9)
+    load_dotenv()
+    auth = OAuth1(os.environ['apikey'], os.environ['secretkey'])
+    apikey = (os.environ['apikey'])
+    endpoint = f"https://api.unsplash.com/photos/?client_id={apikey}"
+    API_response = HTTP_Client.get(endpoint, auth=auth)
+    responseJSON = API_response.json()
+    photo = responseJSON[int(photo_id)]['urls']['small']
+    
+    response = render(request, 'pages/index.html', {'preview_url': photo})
+    return response
+
+def thumb(request):
+    photo_id = request.GET.get('id') or random.randint(1, 9)
+    load_dotenv()
+    auth = OAuth1(os.environ['apikey'], os.environ['secretkey'])
+    apikey = (os.environ['apikey'])
+    endpoint = f"https://api.unsplash.com/photos/?client_id={apikey}"
+    API_response = HTTP_Client.get(endpoint, auth=auth)
+    responseJSON = API_response.json()
+    photo = responseJSON[int(photo_id)]['urls']['thumb']
+    
+    response = render(request, 'pages/index.html', {'preview_url': photo})
     return response
